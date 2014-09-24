3d4f4afede18ad561644d87501cc42168a522836
litten <tengfeiok@gmail.com>
2014-09-24 15:36:31 +0800

    bpm func optimization

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ guitar-beats
 A simple metronome base on node-webkit &amp; phonegap.           
 一个样式简洁的跨平台节拍器.
 
-![metronome](http://littendomo.sinaapp.com/metronome/metronome.jpg)
+![metronome](http://littendomo.sinaapp.com/metronome/metronome.jpg)                
+                
+![metronome2](http://littendomo.sinaapp.com/metronome/metronome2.jpg)  
--- build/js/main.js
+++ build/js/main.js
@@ -1,133 +1,145 @@
-var Main = (function(){
+var Main = (function() {
 
-	var _bpm = 60;
-	var _angle = 0;
-	var _note = 4;
+    var _bpm = 60;
+    var _angle = 0;
+    var _note = 4;
 
-	//二分计数
-	var _trigger = true;
-	//audio是否已初始化
-	var _isAudioInit = false;
-	var _audio;
+    //二分计数
+    var _trigger = true;
+    //audio是否已初始化
+    var _isAudioInit = false;
+    var _audio;
 
-	//是否处于暂停状态
-	var _isPause = true;
-	//是否处于设置bpm状态;
-	var _isSettingBpm = false;
+    //是否处于暂停状态
+    var _isPause = true;
+    //是否处于设置bpm状态;
+    var _isSettingBpm = false;
 
-	var _timeout;
+    var _timeout;
 
-	//不会变动的dom
-	var $point = $(".point");
-	var $btnStatus = $(".btn-status");
-	var $circleOut = $(".circle-out");
-	var $bpmPart = $(".bpm-part");
-	var $beatPart = $(".beat-part");
-	var $settingBpmPart = $(".setting-bpm-part");
-	var $bpmWording = $(".bpm-wording");
-	var $ring = $(".ring");
-	var $tape = $(".tape");
+    //不会变动的dom
+    var $point = $(".point");
+    var $btnStatus = $(".btn-status");
+    var $circleOut = $(".circle-out");
+    var $bpmPart = $(".bpm-part");
+    var $beatPart = $(".beat-part");
+    var $settingBpmPart = $(".setting-bpm-part");
+    var $bpmWording = $(".bpm-wording");
+    var $ring = $(".ring");
+    var $tape = $(".tape");
 
-	//播放声音
-	function playAudio(src) {
-	    if (typeof Audio != "undefined") { 
-	    	if(!_isAudioInit){
-	    		_audio = new Audio(src);
-	    		_audio.play() ;
-	    		_isAudioInit = true;
-	    	}else{
-	    		_audio.play() ;
-	    	}
-	        
-	    } else if (typeof device != "undefined") {
-	        // Android 
-	        if (device.platform == 'Android') {
-	            console.log(src);
-	        }
-	        var mediaRes = new Media(src,function onSuccess() {
-	            mediaRes.release();
-	        },function onError(e){
-	            console.log("error playing sound: " + JSON.stringify(e));
-	        });
-	        mediaRes.play();
-	    } else {
-	        alert("no sound API to play: " + src);
-	    }
-	}
+    //播放声音
+    function playAudio(src) {
+        if (typeof Audio != "undefined") {
+            if (!_isAudioInit) {
+                _audio = new Audio(src);
+                _audio.play();
+                _isAudioInit = true;
+            } else {
+                _audio.play();
+            }
 
-	//旋转指针
-	var toRotate = function(){
-		_angle = (_angle + 360/_note)%360;
-		$point.css({"-webkit-transform": "rotate("+_angle+"deg)"});
-		playAudio("audio/1.mp3");
+        } else if (typeof device != "undefined") {
+            // Android
+            if (device.platform == 'Android') {
+                console.log(src);
+            }
+            var mediaRes = new Media(src, function onSuccess() {
+                mediaRes.release();
+            }, function onError(e) {
+                console.log("error playing sound: " + JSON.stringify(e));
+            });
+            mediaRes.play();
+        } else {
+            alert("no sound API to play: " + src);
+        }
+    }
 
-		if(_angle == 0){
-			$circleOut.addClass("bg-orange");
-			setTimeout(function(){
-				$circleOut.removeClass("bg-orange");
-			},200);
-		}
-		
-		_trigger = !_trigger;
-	}
+    //旋转指针
+    var toRotate = function() {
+        _angle = (_angle + 360 / _note) % 360;
+        $point.css({
+            "-webkit-transform": "rotate(" + _angle + "deg)"
+        });
+        playAudio("audio/1.mp3");
 
-	//事件
-	var bind = function(){
+        if (_angle == 0) {
+            $circleOut.addClass("bg-orange");
+            setTimeout(function() {
+                $circleOut.removeClass("bg-orange");
+            }, 200);
+        }
 
-		//开始or暂停按钮点击
-		$point.bind("touchend", function(e){
-			var $target = $(this);
-			if(_isPause){
-				_timeout = setInterval(toRotate, 60000/_bpm);
-				$btnStatus.removeClass("btn-play").addClass("btn-pause");
-			}else{
-				clearTimeout(_timeout);
-				$btnStatus.removeClass("btn-pause").addClass("btn-play");
-			}
-			_isPause = !_isPause;
-		});
+        _trigger = !_trigger;
+    }
 
-		//设置bpm
-		$bpmPart.bind("touchend", function(e){
-			if(_isSettingBpm){
-				$settingBpmPart.addClass("hide");
-				$beatPart.removeClass("hide");
-			}else{
-				$beatPart.addClass("hide");
-				$settingBpmPart.removeClass("hide");
-				//暂停
-				clearTimeout(_timeout);
-				$btnStatus.removeClass("btn-pause").addClass("btn-play");
-				_isPause = !_isPause;
-			}
-			_isSettingBpm = !_isSettingBpm;
-		});
+    //事件
+    var bind = function() {
 
-		//ring拖动
-		$ring.bind("touchmove", function(e){
-			var offY = e.targetTouches[0].pageY;
-			console.log(offY);
-			//offY 150-600转换到30-240
-			_bpm = parseInt(7*(offY-150)/15+30);
-			if(_bpm < 30){
-				_bpm = 30;
-			}else if(_bpm > 240){
-				_bpm = 240;
-			}
-			$bpmWording.html(_bpm);
-			$tape.css({"height":offY-180});
-		});
+        //开始or暂停按钮点击
+        $point.bind("touchend", function(e) {
+            var $target = $(this);
+            if (_isPause) {
+                _timeout = setInterval(toRotate, 60000 / _bpm);
+                $btnStatus.removeClass("btn-play").addClass("btn-pause");
+            } else {
+                clearTimeout(_timeout);
+                $btnStatus.removeClass("btn-pause").addClass("btn-play");
+            }
+            _isPause = !_isPause;
+        });
 
-		$ring.bind("touchstart", function(e){
-			console.log(e.targetTouches[0].pageY);
-		});
-	}
+        //设置bpm
+        $bpmPart.bind("touchend", function(e) {
+            if (_isSettingBpm) {
+                $settingBpmPart.addClass("hide");
+                $beatPart.removeClass("hide");
+            } else {
+                $beatPart.addClass("hide");
+                $settingBpmPart.removeClass("hide");
+                //暂停
+                clearTimeout(_timeout);
+                $btnStatus.removeClass("btn-pause").addClass("btn-play");
+                _isPause = true;
+            }
+            _isSettingBpm = !_isSettingBpm;
+        });
 
-	return {
-		init: function(){
-			bind();
-		}
-	}
+        //ring拖动
+        $ring.bind("touchmove", function(e) {
+            var offY = e.targetTouches[0].pageY;
+            if (offY < 200) {
+                offY = 200;
+            } else if (offY > 600) {
+                offY = 600;
+            }
+            console.log(offY);
+
+            //offY 200-600转换到30-240， 次方计算
+            _bpm = parseInt(Math.pow(offY - 200, 2)/761.90 + 30);
+
+            if (_bpm < 30) {
+                _bpm = 30;
+            } else if (_bpm > 240) {
+                _bpm = 240;
+            }
+
+            $bpmWording.html(_bpm);
+            $tape.css({
+                "height": offY - 180
+            });
+        });
+
+        $ring.bind("touchstart", function(e) {
+            console.log(e.targetTouches[0].pageY);
+        });
+    }
+
+    return {
+        init: function() {
+            bind();
+        }
+    }
 })();
 
 $(Main.init());
